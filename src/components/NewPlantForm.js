99ad04f5898ab592368dcf921e7ca0af4d0d06d5
Michael-Ngochi <ngoochimike@gmail.com>
2025-04-21 21:29:36 +0300

    Added component functions and props

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onSubmit }) {
+  function handleSubmit(e){
+  e.preventDefault();
+  const form = e.target;
+  const newplant={
+    name: form.elements.name.value,
+    image: form.elements.image.value,
+    price:form.elements.price.value,
+  }
+  onSubmit(newplant);
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,63 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+ const [plants,setPlants]=useState([])
+ const [allPlants, setAllPlants] = useState([]); 
+ const [term,setTerm]=useState('')
+  
+   function handleChange(term){
+    setTerm(term)
+    console.log(`term is ${term}`)
+   }
+
+  function addNew(plant){
+  console.log(plant);
+  // setPlants((prev)=>{
+  //   const newplant={...plant,id: prev.length + 1}
+  //   return[...prev,newplant];
+  // })
+  fetch("http://localhost:6001/plants", {
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/JSON"
+    },
+    body: JSON.stringify(plant)
+  })
+    .then((res) => res.json())
+    .then((newPlantFromServer) => {
+      setPlants((prev) => [...prev, newPlantFromServer]);
+      setAllPlants((prev) => [...prev, newPlantFromServer]);
+    });
+  }
+
+  useEffect(()=>{
+    console.log(`Called`);
+    fetch(`http://localhost:6001/plants`)
+    .then((res)=>res.json())
+    .then((data)=>{
+     setPlants(data);
+     setAllPlants(data)
+     console.log(data);
+    })
+  },[])
+
+
+  useEffect(()=>{
+    setPlants(
+      allPlants.filter(plant =>
+        plant.name.toLowerCase().includes(term.toLowerCase())
+      )
+    );
+  },[term,allPlants])
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onSubmit={addNew}/>
+      <Search onChange={handleChange}/>
+      <PlantList plants={plants}/>
     </main>
   );
 }
